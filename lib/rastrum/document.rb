720aa9f80bb63acbffcb18bde593c4418d46d14a
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-11-12 11:50:42 -0500

    fixes to document class for updating status

--- lib/rastrum/document.rb
+++ lib/rastrum/document.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'date'
-#require 'rastrum'
+
 
 module Rastrum
 	class Document
@@ -18,36 +18,45 @@ module Rastrum
 		end
 
 		def set_edno(ed_no)
-			@doc.xpath('//tei:editionStmt/tei:edition/@n', {"tei" => "http://www.tei-c.org/ns/1.0"}).each do |node|
-        puts filename
-        puts "Old edition was #{node}"
+		@doc.xpath('//tei:editionStmt/tei:edition/@n', {"tei" => "http://www.tei-c.org/ns/1.0"}).each do |node|
+        puts ed_no
         node.content = ed_no    
-        puts "Edition number has been changed to #{ed_no}"
-     	end
+        end
     end
-    def set_date(date)
+    def set_date(date=nil)
     	doc.xpath('//tei:editionStmt/tei:edition/tei:date', {"tei" => "http://www.tei-c.org/ns/1.0"}).each do |node|
-        puts "Old date was #{node.child}"
-        newDate = "#{date}"
+        if date == nil
+            newDate = Date.today.to_s
+        else
+            newDate = date
+        end
         d = Date.parse(newDate)
         formattedDate = d.strftime('%B %d, %Y')
         node['when'] = newDate
         node.content = formattedDate
-        puts "Date was changed to #{formattedDate}"
       end
     end
-    def set_change(date, who, status, ed_no, desc, collab, valscheme)
-    	change = "<change when='#{date}' who='#{who}' status='#{status}' n='#{ed_no}'>
-    							<p>#{desc}</p>
-    							<note type='status'>
-    								<note type='collaboration'>#{collab}</note>
-    								<note type='validating-schema'>#{valscheme}</note>
-    							</note>
-    						</change>"
-    		doc.xpath('//tei:RevisionDesc/listChange', {"tei" => "http://www.tei-c.org/ns/1.0"}).each do |node|
-        	node.prepend_child(change)
-      end
-
+    def set_change(date: nil, who: nil, status: nil, ed_no: nil, desc: nil, collab: nil, valscheme: nil)
+        if date == nil
+            newDate = Date.today.to_s
+        else
+            newDate = date
+        end
+        
+        ## this spacing is important to get (currently desired formatting)
+        ## there is probably a more relable way to do this
+    	change = "
+        <change when='#{newDate}' who='#{who}' status='#{status}' n='#{ed_no}'>
+    			<p>#{desc}</p>
+    			<note type='status'>
+    				<note type='collaboration'>#{collab}</note>
+    				<note type='validating-schema'>#{valscheme}</note>
+    			</note>
+    		</change>"
+    	@doc.xpath('//tei:revisionDesc/tei:listChange', {"tei" => "http://www.tei-c.org/ns/1.0"}).each do |node|
+    	   node.prepend_child(change) 
+        end
+        self.set_status(status)
     end
     def set_status(status)
     	doc.xpath('//tei:RevisionDesc/@status', {"tei" => "http://www.tei-c.org/ns/1.0"}).each do |node|
